66c3b80c81ffdb74ff611b5f7b5e825b22f948c7
driversline <n526258360@gmail.com>
2024-11-01 20:44:23 +0300

    cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -5,7 +5,7 @@ sudo pacman -S --noconfirm cpupower
 echo "Processor information:"
 cpupower frequency-info
 
-echo -n "Select frequency (default 2.4GHz | don't forget to append GHz at the end): "
+echo -n "Select frequency (default 2.4GHz | e.g. 2.67GHz): "
 read frequency
 frequency=${frequency:-2.4GHz}
 
