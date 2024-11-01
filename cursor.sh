65c281b7cf564bbcc4d9d186fd39d9a1c2414e85
driversline <n526258360@gmail.com>
2024-11-01 20:43:08 +0300

    cursor.sh

--- cursor.sh
+++ cursor.sh
@@ -1,24 +1,24 @@
-sudo pacman -S --noconfirm gkt2 gtk3
+#!/bin/bash
+
+sudo pacman -S --noconfirm gtk2 gtk3
 
 mkdir -p ~/.local/share/icons
 
 tar xvf Nordzy-cursors-white.tar.gz -C ~/.local/share/icons
 
-tar xvf Nordzy-cursors-white.tar.gz -C /usr/share/icons
+sudo tar xvf Nordzy-cursors-white.tar.gz -C /usr/share/icons
 
-mkdir ~/.config/gtk-3.0
+mkdir -p ~/.config/gtk-3.0
 
 touch ~/.config/gtk-3.0/settings.ini
 
-echo -e '[Icon Theme]\nInherits=Nordzy-cursors-white' > /usr/share/icons/default/index.theme
+echo -e '[Icon Theme]\nInherits=Nordzy-cursors-white' | sudo tee /usr/share/icons/default/index.theme
 
 touch ~/.gtkrc-2.0
 
 echo 'gtk-cursor-theme-name="Nordzy-cursors-white"' >> ~/.gtkrc-2.0
 
 echo -e '[Settings]\ngtk-cursor-theme-name=Nordzy-cursors-white' >> ~/.config/gtk-3.0/settings.ini
-
-xrdb ~/.Xresources
 
 echo 'xsetroot -cursor_name left_ptr' >> ~/.config/bspwm/bspwmrc
 
