6dedbbfb99c0d0711d4c28b85564a7951ff1674c
driversline <n526258360@gmail.com>
2024-11-09 15:41:05 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ install_package() {
     pacman -Qs "$1" > /dev/null || sudo pacman -S --noconfirm "$1"
 }
 
-packages=(bspwm kitty sxhkd ranger neovim picom feh gnome-keyring zig xorg-xsetroot xorg-xrandr ttf-dejavu)
+packages=(bspwm alacritty sxhkd ranger neovim picom feh gnome-keyring zig xorg-xsetroot xorg-xrandr ttf-dejavu)
 
 for pkg in "${packages[@]}"; do
     install_package "$pkg"
