0cc6b157fc65af71b785ec6a03712705e84dfcab
driversline <n526258360@gmail.com>
2024-11-20 18:37:31 +0300

    Update firefox-bin.sh

--- firefox/firefox-bin.sh
+++ firefox/firefox-bin.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-echo "Установка необходимых зависимостей... (curl, tar, alsa-lib, gtk3)"
 sudo pacman -S --noconfirm curl tar alsa-lib gtk3
 
-cd ~/Downloads || { echo "Не удалось перейти в папку ~/Downloads"; exit 1; }
+cd ~/Downloads || { echo "~/Downloads not found | create to `mkdir ~/Downloads`"; exit 1; }
 
-echo "Загрузка Firefox..."
 curl -L -o firefox.tar.bz2 "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=en-US&_gl=1*1irdmrv*_ga*Nzg1NTczMTE0LjE3Mjk5Mzc4NDA.*_ga_MQ7767QQQW*MTcyOTkzNzg0MC4xLjEuMTcyOTkzODA0MS4wLjAuMA.."
 
 tar xjf firefox.tar.bz2
@@ -14,6 +12,6 @@ sudo mv firefox /opt
 
 sudo ln -s /opt/firefox/firefox /usr/local/bin/firefox
 
-echo "Firefox установлен успешно! Вы можете запустить его, введя 'firefox' в терминале."
+echo "Successfully."
 
 exit 0
