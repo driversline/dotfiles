8b997f1acc7a8dd68e8e61d61eba36edf7228f26
driversline <n526258360@gmail.com>
2024-11-20 18:38:43 +0300

    Update firefox-custom.sh

--- firefox/firefox-custom.sh
+++ firefox/firefox-custom.sh
@@ -18,6 +18,4 @@ sed -i '/padding-left:/s|3|1|' "$HOME/.mozilla/firefox/firefox-themes/WhiteSur/h
 
 sed -i '8a padding-right: 1px !important;' "$HOME/.mozilla/firefox/firefox-themes/WhiteSur/hide-window-buttons.css"
 
-cd
-
 exit 0
