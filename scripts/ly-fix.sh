77e6c9afa3841a30d6746005f6dc8fffc05e0e0e
driversline <n526258360@gmail.com>
2024-12-26 20:13:44 +0300

    ly.sh

--- scripts/ly-fix.sh
+++ scripts/ly-fix.sh
@@ -6,4 +6,6 @@ systemctl disable ly.service
 
 git clone https://github.com/fairyglade/ly && cd ly && zig build && sudo zig build installsystemd && sudo systemctl enable ly.service && sudo systemctl disable getty@tty2.service
 
+echo "Excellent."
+
 exit 0
