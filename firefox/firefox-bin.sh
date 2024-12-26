c10651e71677205919618df0f46f3ead290f3061
driversline <n526258360@gmail.com>
2024-12-26 20:03:17 +0300

    firefox-bin.sh

--- firefox/firefox-bin.sh
+++ firefox/firefox-bin.sh
@@ -2,8 +2,6 @@
 
 sudo pacman -S --noconfirm curl tar alsa-lib gtk3
 
-cd ~/Downloads || { echo "~/Downloads not found | create to `mkdir ~/Downloads`"; exit 1; }
-
 curl -L -o firefox.tar.bz2 "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=en-US&_gl=1*1irdmrv*_ga*Nzg1NTczMTE0LjE3Mjk5Mzc4NDA.*_ga_MQ7767QQQW*MTcyOTkzNzg0MC4xLjEuMTcyOTkzODA0MS4wLjAuMA.."
 
 tar xjf firefox.tar.bz2
@@ -12,6 +10,6 @@ sudo mv firefox /opt
 
 sudo ln -s /opt/firefox/firefox /usr/local/bin/firefox
 
-echo "Successfully."
+echo "Excellent."
 
 exit 0
