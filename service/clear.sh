9e99cab2386901f31b09188e157a00400214e86c
driversline <n526258360@gmail.com>
2025-01-03 14:19:44 +0300

    clear.sh

--- service/clear.sh
+++ service/clear.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-SERVICE_DIR=~/bspwm/service
+if ! command -v fstrim &> /dev/null; then
+    echo "Ошибка: fstrim не установлен. Установите его и попробуйте снова."
+    exit 1
+fi
+
+SERVICE_DIR=$HOME/bspwm/service
 
 sudo cp $SERVICE_DIR/cl.service /etc/systemd/system/
 sudo cp $SERVICE_DIR/cl.timer /etc/systemd/system/
