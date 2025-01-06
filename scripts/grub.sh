d15153c94f334574bfa11e3f289984456eab2b1d
driversline <n526258360@gmail.com>
2025-01-06 22:16:15 +0300

    grub.sh

--- scripts/grub.sh
+++ scripts/grub.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+set -e
+
 GRUB_CONFIG="/etc/default/grub"
 
-sudo nvim +"/^GRUB_TIMEOUT=/s/GRUB_TIMEOUT=[0-9]*/GRUB_TIMEOUT=1/" +wq "$GRUB_CONFIG"
+update_grub_timeout() {
+    sudo nvim +"/^GRUB_TIMEOUT=/s/GRUB_TIMEOUT=[0-9]*/GRUB_TIMEOUT=1/" +wq "$GRUB_CONFIG"
+}
 
-sudo grub-mkconfig -o /boot/grub/grub.cfg
+generate_grub_config() {
+    sudo grub-mkconfig -o /boot/grub/grub.cfg
+}
 
-echo "Excellent."
+main() {
+    update_grub_timeout
+    generate_grub_config
+}
+
+main
+
+exit 0
