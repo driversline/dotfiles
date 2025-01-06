73a55f60a242cf75e1aa1dac5ff10c5199e26f5d
driversline <n526258360@gmail.com>
2025-01-06 22:03:23 +0300

    custom.sh

--- scripts/bash/custom.sh
+++ scripts/bash/custom.sh
@@ -1,10 +1,30 @@
 #!/bin/sh
 
+set -e
+
 HOME_DIR="$HOME"
 BASHRC_DIR="$HOME/bspwm/scripts/bash"
+BASHRC_FILE=".bashrc"
 
-rm -f "$HOME_DIR/.bashrc"
+remove_file() {
+    local file_path="$1"
+    if [ -f "$file_path" ]; then
+        rm -f "$file_path"
+    else
+        echo "File $file_path does not exist."
+    fi
+}
 
-mv "$BASHRC_DIR/.bashrc" "$HOME_DIR/.bashrc"
+move_file() {
+    local src="$1"
+    local dest="$2"
+    if [ -f "$src" ]; then
+        mv "$src" "$dest"
+    else
+        echo "Source file $src does not exist."
+    fi
+}
 
-source "$HOME_DIR/.bashrc"
+remove_file "$HOME_DIR/$BASHRC_FILE"
+move_file "$BASHRC_DIR/$BASHRC_FILE" "$HOME_DIR/$BASHRC_FILE"
+. "$HOME_DIR/$BASHRC_FILE"
