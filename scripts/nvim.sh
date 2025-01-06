ab3970b91a0e979ef32c6473ea503ced2d4e9513
driversline <n526258360@gmail.com>
2025-01-06 22:19:20 +0300

    nvim.sh

--- scripts/nvim.sh
+++ scripts/nvim.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 declare -A files_to_move=(
     ["$HOME/bspwm/scripts/nvim/init.vim"]="$HOME/.config/nvim"
     ["$HOME/bspwm/scripts/nvim/nvim-theme.vim"]="$HOME/.config/nvim/colors"
@@ -10,11 +12,24 @@ directories=(
     "$HOME/.config/nvim/colors"
 )
 
-for dir in "${directories[@]}"; do
-    mkdir -p "$dir"
-done
+create_directories() {
+    for dir in "${directories[@]}"; do
+        mkdir -p "$dir"
+    done
+}
 
-for src in "${!files_to_move[@]}"; do
-    dest="${files_to_move[$src]}"
-    mv "$src" "$dest"
-done
+move_files() {
+    for src in "${!files_to_move[@]}"; do
+        dest="${files_to_move[$src]}"
+        mv "$src" "$dest"
+    done
+}
+
+main() {
+    create_directories
+    move_files
+}
+
+main
+
+exit 0
