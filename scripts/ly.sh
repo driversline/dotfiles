8d38cbae024741122dffcdb4475cc3b2ca030cdb
driversline <n526258360@gmail.com>
2025-01-06 22:17:27 +0300

    ly.sh

--- scripts/ly.sh
+++ scripts/ly.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
 
-sudo pacman -S --noconfirm zig
+set -e
 
-systemctl disable ly.service
+install_zig() {
+    sudo pacman -S --noconfirm zig
+}
 
-git clone https://github.com/fairyglade/ly && cd ly && zig build && sudo zig build installsystemd && sudo systemctl enable ly.service && sudo systemctl disable getty@tty2.service
+disable_ly_service() {
+    systemctl disable ly.service
+}
 
-echo "Excellent."
+clone_and_build_ly() {
+    git clone https://github.com/fairyglade/ly
+    cd ly
+    zig build
+    sudo zig build installsystemd
+}
+
+enable_ly_service() {
+    sudo systemctl enable ly.service
+    sudo systemctl disable getty@tty2.service
+}
+
+main() {
+    install_zig
+    disable_ly_service
+    clone_and_build_ly
+    enable_ly_service
+}
+
+main
 
 exit 0
