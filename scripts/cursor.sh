879bb9b1925e055dc156513ded6c9f2c1092a1a0
driversline <n526258360@gmail.com>
2025-01-14 19:40:21 +0300

    cursor.sh

--- scripts/cursor.sh
+++ scripts/cursor.sh
@@ -2,45 +2,28 @@
 
 set -e
 
-install_packages() {
-    sudo pacman -S --noconfirm gtk2 gtk3
-}
+#!/bin/bash
 
-extract_cursors() {
-    local cursor_archive="$HOME/bspwm/cursors/Nordzy-cursors-white.tar.gz"
-    local local_icons_dir="$HOME/.local/share/icons"
-    local system_icons_dir="/usr/share/icons"
-    
-    mkdir -p "$local_icons_dir"
-    tar xvf "$cursor_archive" -C "$local_icons_dir"
-    
-    sudo tar xvf "$cursor_archive" -C "$system_icons_dir"
-}
+sudo pacman -S --noconfirm gtk2 gtk3
 
-configure_gtk() {
-    local gtk3_config_dir="$HOME/.config/gtk-3.0"
-    local gtk2_config_file="$HOME/.gtkrc-2.0"
-    local cursor_theme="Nordzy-cursors-white"
-    
-    mkdir -p "$gtk3_config_dir"
-    echo -e '[Settings]\ngtk-cursor-theme-name='$cursor_theme >> "$gtk3_config_dir/settings.ini"
-    
-    echo 'gtk-cursor-theme-name="'$cursor_theme'"' >> "$gtk2_config_file"
-}
+mkdir -p ~/.local/share/icons
 
-configure_bspwm() {
-    local bspwm_config_file="$HOME/.config/bspwm/bspwmrc"
-    
-    echo 'xsetroot -cursor_name left_ptr' >> "$bspwm_config_file"
-}
+tar xvf Nordzy-cursors-white.tar.gz -C ~/.local/share/icons
 
-main() {
-    install_packages
-    extract_cursors
-    configure_gtk
-    configure_bspwm
-}
+sudo tar xvf Nordzy-cursors-white.tar.gz -C /usr/share/icons
 
-main
+mkdir -p ~/.config/gtk-3.0
+
+touch ~/.config/gtk-3.0/settings.ini
+
+echo -e '[Icon Theme]\nInherits=Nordzy-cursors-white' | sudo tee /usr/share/icons/default/index.theme
+
+touch ~/.gtkrc-2.0
+
+echo 'gtk-cursor-theme-name="Nordzy-cursors-white"' >> ~/.gtkrc-2.0
+
+echo -e '[Settings]\ngtk-cursor-theme-name=Nordzy-cursors-white' >> ~/.config/gtk-3.0/settings.ini
+
+echo 'xsetroot -cursor_name left_ptr' >> ~/.config/bspwm/bspwmrc
 
 exit 0
