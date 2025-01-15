cae20f92017eb32afed627232b69dfb006b600aa
driversline <n526258360@gmail.com>
2025-01-15 20:17:46 +0300

    cursor.sh

--- scripts/cursor.sh
+++ scripts/cursor.sh
@@ -6,9 +6,9 @@ sudo pacman -S --noconfirm gtk2 gtk3
 
 mkdir -p ~/.local/share/icons
 
-tar xvf $HOME/bspwm/cursors/Nordzy-cursors-white.tar.gz -C ~/.local/share/icons
+tar xvf $HOME/bspwm/dotfiles/cursors/Nordzy-cursors-white.tar.gz -C ~/.local/share/icons
 
-sudo tar xvf $HOME/bspwm/cursors/Nordzy-cursors-white.tar.gz -C /usr/share/icons
+sudo tar xvf $HOME/bspwm/dotfiles/cursors/Nordzy-cursors-white.tar.gz -C /usr/share/icons
 
 mkdir -p ~/.config/gtk-3.0
 
