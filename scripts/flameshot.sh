dadbf073e13ae0bb599a59c488008e4a957bcb46
driversline <n526258360@gmail.com>
2025-01-17 16:58:59 +0300

    flameshot.sh

---
+++ scripts/flameshot.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+declare -A files_to_move=(
+    ["$HOME/bspwm/dotfiles/flameshot.ini"]="$HOME/.config/flameshot"
+)
+
+directories=(
+    "$HOME/.config/flameshot"
+)
+
+create_directories() {
+    for dir in "${directories[@]}"; do
+        mkdir -p "$dir"
+    done
+}
+
+move_files() {
+    for src in "${!files_to_move[@]}"; do
+        dest="${files_to_move[$src]}"
+        mv "$src" "$dest"
+    done
+}
+
+main() {
+    create_directories
+    move_files
+}
+
+main
+
+exit 0
