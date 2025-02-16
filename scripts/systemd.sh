376c4e78bdcd6946becd43e18c05e9db9095c6cd
driversline <n526258360@gmail.com>
2025-02-16 12:30:18 +0300

    systemd.sh

--- scripts/systemd.sh
+++ scripts/systemd.sh
@@ -1,1 +1,23 @@
-~/.config/systemd/user/firefox.service todo
+#!/usr/bin/env fish
+
+set -e
+
+set target "$HOME/bspwm/dotfiles/firefox.service"
+set directory "$HOME/.config/systemd/user/"
+
+function create_directory
+    mkdir -p $directory
+end
+
+function move_file
+    mv $target $directory
+end
+
+function main
+    create_directory
+    move_file
+end
+
+main
+
+exit 0
